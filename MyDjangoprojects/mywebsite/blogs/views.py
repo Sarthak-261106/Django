b37fa569fc30157c49edf837bc091e0c095c0dfa
Sarthak-261106 <sarthak.neema2606@gmail.com>
2026-08-03 20:20:32 +0530

    django

--- MyDjangoprojects/mywebsite/blogs/views.py
+++ MyDjangoprojects/mywebsite/blogs/views.py
@@ -3,21 +3,29 @@ from django.http import HttpResponse,HttpResponseNotFound
 from django.shortcuts import render
 
 # Create your views here.
+
+blog_names={'python-intro':"python blog posts!",'django-basics':"django blog posts!",'regex':'regular expressions'}
+
 def home_page(request):
     return HttpResponse("<h1>Home page of website blog</h1>")
+
+
 def blogposts(request):
     return  HttpResponse("all blog posts!")
 
 
 def blog_post(request, blog):
-    if blog=='python-intro':
-        res = "python blog posts!"
-    elif blog=='django-basics':
-        res = "django blog posts!"
-    else:
+    # if blog=='python-intro':
+    #     res = "python blog posts!"
+    # elif blog=='django-basics':
+    #     res = "django blog posts!"
+    # else:
+    try:
+        res=blog_names[blog]
+    except Exception:
         return HttpResponseNotFound('Blog not found')
-
-    return HttpResponse(res)
+    else:
+        return HttpResponse(res)
 
 def blog_post_by_number(request, blog):
     return HttpResponse(blog)
