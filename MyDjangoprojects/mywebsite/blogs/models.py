2e07eb37207658b306ac624ebf984d6ae9890d08
Sarthak-261106 <sarthak.neema2606@gmail.com>
2026-08-21 23:56:32 +0530

    django

--- MyDjangoprojects/mywebsite/blogs/models.py
+++ MyDjangoprojects/mywebsite/blogs/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    preview = models.CharField(max_length=250)
+    content = models.TextField()
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(unique=True)
+    image = models.CharField(max_length=50)
+
+
