ed3d7e685a255487a4e01e3e117a36c2792d2688
Sarthak-261106 <sarthak.neema2606@gmail.com>
2026-08-22 15:35:30 +0530

    django

--- MyDjangoprojects/mywebsite/blogs/models.py
+++ MyDjangoprojects/mywebsite/blogs/models.py
@@ -2,6 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email_addr = models.EmailField()
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=20)
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
     preview = models.CharField(max_length=250)
@@ -9,5 +17,8 @@ class Post(models.Model):
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True)
     image = models.CharField(max_length=50)
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True)
+    tags = models.ManyToManyField(Tag)
+
 
 
