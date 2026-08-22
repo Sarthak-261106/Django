73995da508420d6ec79455a753701f546e42af4c
Sarthak-261106 <sarthak.neema2606@gmail.com>
2026-08-22 21:59:33 +0530

    django

--- MyDjangoprojects/mywebsite/blogs/models.py
+++ MyDjangoprojects/mywebsite/blogs/models.py
@@ -7,8 +7,17 @@ class Author(models.Model):
     last_name = models.CharField(max_length=50)
     email_addr = models.EmailField()
 
+    def get_full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def __str__(self):
+        return self.get_full_name()
+
 class Tag(models.Model):
     caption = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.caption
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
@@ -19,6 +28,9 @@ class Post(models.Model):
     image = models.CharField(max_length=50)
     author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True)
     tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.title
 
 
 
