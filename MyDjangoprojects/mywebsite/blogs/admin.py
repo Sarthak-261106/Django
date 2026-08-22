529c4f8190b1e2844506754f0ba67370f02adc1b
Sarthak-261106 <sarthak.neema2606@gmail.com>
2026-08-22 22:15:37 +0530

    django

--- MyDjangoprojects/mywebsite/blogs/admin.py
+++ MyDjangoprojects/mywebsite/blogs/admin.py
@@ -2,6 +2,16 @@ from django.contrib import admin
 from .models import Post,Author,Tag
 # Register your models here.
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title','author','date','tags')
+    list_filter = ('author','date','tags')
+    search_fields = ('title','author','date','tags')
+    prepopulated_fields = {'slug':('title',)}
+
+
+
+admin.site.register(Post,PostAdmin)
 admin.site.register(Author)
 admin.site.register(Tag)
+
+
